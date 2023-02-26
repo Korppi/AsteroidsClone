3990b985da8f6207f78ae11e48b40f36781ae0c6
Korppi <3602291+Korppi@users.noreply.github.com>
2023-02-26 14:59:58 +0200

    player can now shoot lasers

--- scenes/game/player/PlayerFakePhysics.gd
+++ scenes/game/player/PlayerFakePhysics.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var LASER = preload("res://scenes/game/Laser/Laser.tscn")
+
 var vel = Vector2.ZERO
 
 func _process(delta):
@@ -11,4 +13,9 @@ func _process(delta):
 		vel = lerp(vel, transform.y * -10, 5 * delta)
 	else:
 		vel = lerp(vel, Vector2.ZERO, 1 * delta)
+	if Input.is_action_just_pressed("fire_laser"):
+		var l = LASER.instance()
+		l.position = $Gun.global_position
+		l.rotation = rotation
+		$Lasers.add_child(l)
 	position += vel
